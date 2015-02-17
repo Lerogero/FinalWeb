50d04dcde84350d0f6c5629aa7895c628c72eafc
Grant <Lerogero@gmail.com>
2015-02-17 16:25:05 +0000

    Fixed Conflicts

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -13,18 +13,12 @@
           <div class="row">
               <div class="col-xs-12">
                   <div class="form-wrap">
-                        <form role="form" action="javascript:;" method="post" id="login-form" autocomplete="off">
+                        <form role="form"  method="post" id="	login-form" autocomplete="off">
                             <div class="form-group">
                                 <label for="username" class="sr-only">Username</label>
-<<<<<<< HEAD
                                 <input type="username" name="email" id="email" class="form-control" placeholder="Username" autofocus>
-                              </div>
-                              <div class="form-group">
-=======
-                                <input  type="username" name="email" id="email" class="form-control" placeholder="Username" autofocus>
-                            </div>
-                            <div class="form-group">
->>>>>>> origin/master
+								                </div>
+                                <div class="form-group">
                                 <label for="key" class="sr-only">Password</label>
                                 <input type="password" name="key" id="key" class="form-control" placeholder="Password">
                             </div>
