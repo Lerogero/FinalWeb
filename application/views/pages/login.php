06ac22741c32865d0e98f0280dab6a192f601054
Grant <Lerogero@gmail.com>
2015-02-17 14:45:44 +0000

    Fixed Some web things updated legin

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -16,18 +16,18 @@
                         <form role="form" action="javascript:;" method="post" id="login-form" autocomplete="off">
                             <div class="form-group">
                                 <label for="username" class="sr-only">Username</label>
-                                <input type="username" name="email" id="email" class="form-control" placeholder="Username">
-                            </div>
-                            <div class="form-group">
+                                <input type="username" name="email" id="email" class="form-control" placeholder="Username" autofocus>
+                              </div>
+                              <div class="form-group">
                                 <label for="key" class="sr-only">Password</label>
                                 <input type="password" name="key" id="key" class="form-control" placeholder="Password">
                             </div>
                             <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Log in">
                         </form>
                   </div>
-            </div> <!-- /.col-xs-12 -->
-          </div> <!-- /.row -->
-        </div> <!-- /.container -->
+            </div>
+          </div>
+        </div>
     </section>
     <script src="<?php echo site_url('application/views/js/bootstrap.min.js')?>"></script>
   </body>
